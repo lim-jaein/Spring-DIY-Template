306949d440818bfbf0f4174dfdf97f887acec2e0
junseo <djawnstj44@naver.com>
2026-03-02 00:53:50 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("java")
+    kotlin("jvm")
+}
+
+group = "com.diy"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // ObjectMapper
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
+
+    // 서블릿
+    implementation("javax.servlet:javax.servlet-api:4.0.1")
+    implementation("javax.servlet:jstl:1.2")
+
+    // 톰캣
+    implementation("org.apache.tomcat.embed:tomcat-embed-core:8.5.42")
+    implementation("org.apache.tomcat.embed:tomcat-embed-jasper:8.5.42")
+
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/com/diy/app/Main.java
@@ -0,0 +1,7 @@
+package com.diy.app;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
