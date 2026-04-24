5fc13d5e821e6377c1dc9e6054dd83a5554e43c1
201842 <limjaein@shinsegae.com>
2026-04-24 16:38:50 +0900

    refactor: 인터페이스 기반 컨트롤러 method 제거

--- src/main/java/com/diy/app/login/LoginController.java
+++ src/main/java/com/diy/app/login/LoginController.java
@@ -3,7 +3,6 @@ package com.diy.app.login;
 import com.diy.framework.web.Controller;
 import com.diy.framework.web.annotation.Component;
 import com.diy.framework.web.annotation.RequestMapping;
-import com.diy.framework.web.annotation.RequestMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,12 +10,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Component
-@RequestMapping(methods = RequestMethod.GET, value = "/login")
+@RequestMapping(value = "/login")
 public class LoginController implements Controller {
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
 
     @Override
     public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         log.debug("loginController test");
+        // 인터페이스 방식은 내부에서 http method 분기처리 필요
     }
 }
